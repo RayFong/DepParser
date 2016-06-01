667a618888360a2abcef7377752c2d525cc7325c
RayFong <leifengpku@163.com>
2016-06-01 05:17:49 +0000

    Add files via upload

--- DepLabel.py
+++ DepLabel.py
@@ -13,85 +13,70 @@ class DepLabel(Perceptron):
 		]	
 		feature_names = [
 			'p_word', 'p_pos', 's_word', 's_pos',
+			'gp_word', 'gp_pos', 'gp_word_pos',
 			'p_word_pos', 's_word_pos', 
 			'p_s_words', 'p_s_poses',
-			'qp_word', 'qp_pos', 'np_word', 'np_pos',
-			'qs_word', 'qs_pos', 'ns_word', 'ns_pos',
-			'qp_word_pos', 'np_word_pos', 'qs_word_pos', 'ns_word_pos',
-			'qp_p_poses', 'p_np_poses',
-			'qs_s_poses', 's_ns_poses',
-			'qp_p_np_poses', 'qs_s_ns_poses',
+			'gp_p_words', 'gp_p_poses',
+			'gp_p_s_poses',
+			'gp_children', 'p_children', 's_children',
 			'p_s_distance', 'p_s_dir'
 		]
 		Perceptron.__init__(self, self.total_labels, feature_names)
 		
-	def extract_feature(self, sentence, s):
+	def get_children_num(self, sentence):
+		children_num = [0] * len(sentence)
+		for item in sentence:
+			p = int(item[self.HEAD])
+			if p != -1:
+				children_num[p] += 1
+		return children_num
+		
+	def extract_feature(self, sentence, children_num, s):
 		s_len = len(sentence)
 		p = int(sentence[s][self.HEAD])
-		
-		if p > 0:
-			qp_word = sentence[p-1][self.WORD]
-			qp_pos = sentence[p-1][self.POS]
-		else:
-			qp_word = '_NULL_'
-			qp_pos = '_NULL_'	
+		gp = int(sentence[p][self.HEAD])
+
 		p_word = sentence[p][self.WORD]
-		p_pos = sentence[p][self.POS]	
-		if p+1 < s_len:
-			np_word = sentence[p+1][self.WORD]
-			np_pos = sentence[p+1][self.POS]
-		else:
-			np_word = '_NULL_'
-			np_pos = '_NULL_'
-			
-		if s > 0:
-			qs_word = sentence[s-1][self.WORD]
-			qs_pos = sentence[s-1][self.POS]
-		else:
-			qs_word = '_NULL_'
-			qs_pos = '_NULL_'			
+		p_pos = sentence[p][self.POS]
+		p_children = children_num[p]
+		
 		s_word = sentence[s][self.WORD]
-		s_pos = sentence[s][self.POS]			
-		if s+1 < s_len:
-			ns_word = sentence[s+1][self.WORD]
-			ns_pos = sentence[s+1][self.POS]
+		s_pos = sentence[s][self.POS]
+		s_children = children_num[s]
+			
+		if gp != -1:
+			gp_word = sentence[gp][self.WORD]
+			gp_pos = sentence[gp][self.POS]
+			gp_children = children_num[gp]
 		else:
-			ns_word = '_NULL_'
-			ns_pos = '_NULL_'
+			gp_word = '_NULL_'
+			gp_pos = '_NULL_'
+			gp_children = -1
 			
 		feature = {}
 		
-		feature['qp_word'] = qp_word
 		feature['p_word'] = p_word
-		feature['np_word'] = np_word
-		feature['qp_pos'] = qp_pos
 		feature['p_pos'] = p_pos
-		feature['np_pos'] = np_pos
+		feature['p_word_pos'] = p_word + '|' + p_pos
 		
-		feature['qs_word'] = qs_word
 		feature['s_word'] = s_word
-		feature['ns_word'] = ns_word
-		feature['qs_pos'] = qs_pos
 		feature['s_pos'] = s_pos
-		feature['ns_pos'] = ns_pos
+		feature['s_word_pos'] = s_word + '|' + s_pos
+		
+		feature['gp_word'] = gp_word
+		feature['gp_pos'] = gp_pos
+		feature['gp_p_words'] = gp_word + '|' + p_word
+		feature['gp_word_pos'] = gp_word + '|' + gp_pos
 		
 		feature['p_s_words'] = p_word + '|' + s_word
 		feature['p_s_poses'] = p_pos + '|' + s_pos
+		feature['gp_p_poses'] = gp_pos + '|' + p_pos
 		
-		feature['qp_word_pos'] = qp_word + '|' + qp_pos
-		feature['p_word_pos'] = p_word + '|' + p_pos
-		feature['np_word_pos'] = np_word + '|' + np_pos
+		feature['gp_children'] = gp_children
+		feature['p_children'] = p_children
+		feature['s_children'] = s_children
 		
-		feature['qs_word_pos'] = qs_word + '|' + qs_pos
-		feature['s_word_pos'] = s_word + '|' + s_pos
-		feature['ns_word_pos'] = ns_word + '|' + ns_pos
-		
-		feature['qp_p_poses'] = qp_pos + '|' + p_pos
-		feature['p_np_poses'] = p_pos + '|' + np_pos
-		feature['qs_s_poses'] = qs_pos + '|' + s_pos
-		feature['s_ns_poses'] = s_pos + '|' + ns_pos
-		feature['qp_p_np_poses'] = qp_pos + '|' + p_pos + '|' + np_pos
-		feature['qs_s_ns_poses'] = qs_pos + '|' + s_pos + '|' + ns_pos
+		feature['gp_p_s_poses'] = gp_pos + '|' + p_pos + '|' + s_pos
 		
 		if p > s:
 			feature['p_s_dir'] = 'L'
@@ -108,9 +93,10 @@ class DepLabel(Perceptron):
 		sent_iter = read_file(trn_file)
 		for sent in sent_iter:
 			sent_feat_vec, labels = [], []
+			children_num = self.get_children_num(sent)
 			for idx in range(len(sent)):
 				if sent[idx][self.HEAD] != '-1':
-					feat_vec = self.extract_feature(sent, idx)
+					feat_vec = self.extract_feature(sent, children_num, idx)
 					sent_feat_vec.append(feat_vec)
 					labels.append(sent[idx][self.LABEL])
 			all_feat_vec.append(sent_feat_vec)
@@ -121,12 +107,13 @@ class DepLabel(Perceptron):
 		
 	def predict(self, sentence):
 		predict_sentence = []
+		children_num = self.get_children_num(sentence)
 		
 		for idx, item in enumerate(sentence):
 			if item[self.HEAD] == '-1':
 				predict_sentence.append([item[0], item[1], item[2], 'ROOT'])
 			else:
-				feat_vec = self.extract_feature(sentence, idx)
+				feat_vec = self.extract_feature(sentence, children_num, idx)
 				predict_label = self.predict_t(feat_vec, self.total_labels)
 				predict_sentence.append([item[0], item[1], item[2], predict_label])
 				
@@ -144,5 +131,5 @@ class DepLabel(Perceptron):
 		
 if __name__ == '__main__':
 	depLabel = DepLabel()
-	depLabel.train('d:\\Project1\\data\\trn.ec')
-	depLabel.predict_and_save('d:\\Project1\\data\\dev.ec', 'd:\\Project1\\result_3.ec')
+	depLabel.train('d:\\Project1\\data\\trn.ec', 10)
+	depLabel.predict_and_save('d:\\Project1\\data\\dev.ec', 'd:\\Project1\\ans3.ec')
